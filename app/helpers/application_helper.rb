9944ccc1367983e54fdd6fc4f9d30a398cdb0297
swiecone <alejandro.swiec@gmail.com>
2016-04-24 11:06:42 +0200

    Added a feature that returns nil if Google API does not work

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,12 +3,21 @@ module ApplicationHelper
 	def book_thumbnail(book)
 	   	 b = GoogleBook::Book.new(:api_key => "AIzaSyDE0VW_-8EjjPrUJgYbZpjBylZS0w-wj2M") 
     	 b.search("#{book.isbn}",5) 
- 	     b.books.first.small_thumbnail_link
+    	 if b == nil
+    	 	return nil
+    	 else 
+ 	     	b.books.first.small_thumbnail_link
+ 	     end 
 	end
 
 	def book_title(book)
 		 b = GoogleBook::Book.new(:api_key => "AIzaSyDE0VW_-8EjjPrUJgYbZpjBylZS0w-wj2M") 
    		 b.search("#{book.isbn}",5) 
- 		 b.books.first.get_title
+ 		
+ 		 if b == nil
+    	 	return nil
+    	 else 
+    	 	return  b.books.first.get_title
+    	 end 
 	end 
 end

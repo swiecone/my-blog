d5b3b5d8456c6fcd926afb8e9f2f29b3b741f6d6
swiecone <alejandro.swiec@gmail.com>
2016-04-30 22:24:27 +0200

    added error messages in case new book is not correctly added

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ActiveRecord::Base
-	validates :isbn, presence: true 
-	validates :title, presence: true 
-	validates :shortsummary, presence: true 
-	validates :thumburl, presence: true  
+	validates :isbn, presence: true ,  length: { minimum: 10, maximum: 13 }
+	validates :title, presence: true , length: { minimum: 5, maximum: 100 }
+	validates :shortsummary, presence: true, length: { minimum: 30, maximum: 400 }
+	validates :thumburl, presence: true ,  length: { minimum: 10 }
 end
 

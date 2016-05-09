8117141c742cbc4d0c7f8b396327c1552fb9f6f4
swiecone <alejandro.swiec@gmail.com>
2016-05-09 22:16:14 +0200

    created a new model called user to control access to different parts of the page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160508103414) do
+ActiveRecord::Schema.define(version: 20160509201418) do
 
   create_table "books", force: :cascade do |t|
     t.string   "isbn"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 20160508103414) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "email"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.boolean  "admin"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end

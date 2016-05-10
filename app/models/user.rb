7dd36381383fc63c1b346c70013e5fe94587af0b
swiecone <alejandro.swiec@gmail.com>
2016-05-11 00:14:21 +0200

    Added user validation in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+	before_save { self.email = email.downcase }
+	validates :name, presence: true, length: { minimum: 3, maximum: 40}, 
+				uniqueness: { case_sensitive: false }
+
+	VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+	validates :email, presence: true, length: {maximum: 105},
+							uniqueness: { case_sensitive: false },
+							format: { with: VALID_EMAIL_REGEX }
 end

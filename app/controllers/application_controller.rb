4e59e401127f0d010836fc5033cefa0e801f233c
swiecone <alejandro.swiec@gmail.com>
2016-05-18 11:16:12 +0200

    modified the new model inside mooc and also blocked access to new and create so only logged in users can input new moocs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,8 @@ class ApplicationController < ActionController::Base
 
   def require_user
   	if !logged_in?
-  		flash[:danger] = "You must be logged in to perform this action"
+      @login = "<a href='#''>here</a>"
+  		flash[:danger] = "You must be logged in to perform this action."
   		redirect_to root_path
   	end 
   end 
--- app/controllers/moocs_controller.rb
+++ app/controllers/moocs_controller.rb
@@ -1,4 +1,5 @@
 class MoocsController < ApplicationController
+  before_action :require_user, only: [:new, :create]
 
 	def index
 	end 

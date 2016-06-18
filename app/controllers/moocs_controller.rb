1f50c13e5f9e7d798638580585749fe8d3003a8e
swiecone <alejandro.swiec@gmail.com>
2016-06-18 10:24:19 -0300

    trying my best to make new mooc work

--- app/controllers/moocs_controller.rb
+++ app/controllers/moocs_controller.rb
@@ -14,8 +14,32 @@ class MoocsController < ApplicationController
 		@institutions = Institution.all
 	end 
 
+	def create 
+    @mooc = Mooc.new(moocs_params)
+    @mooc.institution_id = params[:institution_id]
+    @mooc.platform_id = params[:platform_id]
+    @mooc.rating = params[:rating]
+    @mooc.url = params[:url]
+
+    @mooc.thumburl = params[:thumburl]
+    debugger
+
+    if @mooc.save
+      flash[:success] = "Your MOOC was created successfully!"
+      redirect_to moocs_path
+    else
+      render :new
+    end
+  end 
+
+
 	def show 
 		@mooc = Mooc.find(1)
 	end
+
+private
+  def moocs_params
+    params.require(:mooc).permit( :title, :platform, :institution, :shortsummary, :status, :rating, :url, :thumburl)
+  end
 
 end 

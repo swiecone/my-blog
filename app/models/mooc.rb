2d6da71dbfa8c67aa83b61c8fd7eb4dce7428855
swiecone <alejandro.swiec@gmail.com>
2016-06-20 23:20:15 -0300

    added mooc model and modified the mooc index

--- app/models/mooc.rb
+++ app/models/mooc.rb
@@ -7,5 +7,4 @@ class Mooc < ActiveRecord::Base
    validates :shortsummary, presence: true ,  length: { minimum: 30, maximum: 400 }
    validates :platform_id, presence: true 
    validates :institution_id, presence: true  
-   validates :certification, presence: true, length: { minimum: 2, maximum: 400 }
 end

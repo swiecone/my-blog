f9fb673171361bf67dd381565483614e4ce7ce19
swiecone <alejandro.swiec@gmail.com>
2016-06-28 23:51:15 +0200

    trying again to  fix issue regarding pushing code to heroku

--- db/migrate/20160616012521_remove_institutionid_from_moocs.rb
+++ db/migrate/20160616012521_remove_institutionid_from_moocs.rb
@@ -1,5 +1,5 @@
 class RemoveInstitutionidFromMoocs < ActiveRecord::Migration
   def change
-  	  	remove_column :moocs, :institution_id, :string
+  	  	#remove_column :moocs, :institution_id, :string
   end
 end

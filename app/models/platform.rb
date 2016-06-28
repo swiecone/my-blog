4b61a3f23be626b58d0afbb01e9d7092ea209931
swiecone <alejandro.swiec@gmail.com>
2016-06-28 23:29:42 +0200

    Updated the platform rspec to fit tests

--- app/models/platform.rb
+++ app/models/platform.rb
@@ -1,6 +1,6 @@
 class Platform < ActiveRecord::Base
 	has_many :moocs
 	validates :name, presence: true ,  length: { minimum: 3, maximum: 50 }
-	validates :url, presence: true ,  length: { minimum: 3 }
+	validates :url, presence: true ,  length: { minimum: 11 }
 end
 
--- spec/models/platform_spec.rb
+++ spec/models/platform_spec.rb
@@ -1,19 +1,46 @@
 require 'rails_helper'
 
- # create_table "platforms", force: :cascade do |t|
- #    t.string   "name"
- #    t.string   "url"
- #    t.datetime "created_at", null: false
- #    t.datetime "updated_at", null: false
- #  end
+ 
 
 describe Platform do
   it "is valid with name and url" do 
-  	platform = Platform.new(name: "Coursera",
+  	platform = Platform.create(name: "Coursera",
   							url: "https://www.coursera.org/")
     expect(platform).to be_valid
   end 
 
-   it "is invalid without name"
-   it "is invalid without url"
+   it "is invalid without name" do 
+	platform = Platform.create(name: nil,
+  							url: "https://www.coursera.org/")
+    expect(platform.errors[:name]).to include( "can't be blank" , "is too short (minimum is 3 characters)") 
+   end 
+
+   it "is invalid with a name of less than 3 characters" do 
+	platform = Platform.create(name: "AA",
+  							url: "https://www.coursera.org/")
+    expect(platform.errors[:name]).to include("is too short (minimum is 3 characters)") 
+   end 
+
+    it "is invalid with a name of more than 50 characters" do 
+	long_name = "A" * 51
+	platform = Platform.create(name: long_name,
+  							url: "https://www.coursera.org/")
+    expect(platform.errors[:name]).to include("is too long (maximum is 50 characters)") 
+   end 
+
+   it "is invalid without url" do 
+	platform = Platform.create(name: "Coursera",
+  							url: nil)
+    expect(platform.errors[:url]).to include( "can't be blank" , "is too short (minimum is 11 characters)") 
+   end 
+
+    it "is invalid too short url" do 
+	platform = Platform.create(name: "Coursera",
+  							url: "http://www")
+    expect(platform.errors[:url]).to include( "is too short (minimum is 11 characters)") 
+   end 
+
 end
+
+
+

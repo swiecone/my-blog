7e193cb6e642391a69a19cc9dd9cb33b5b1c71a1
swiecone <alejandro.swiec@gmail.com>
2016-07-01 23:42:44 +0200

    Added a spec for names bigger than 40 for users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,7 +16,11 @@ describe User do
 		 user = User.create(name: "AA", email: "alex3@alexswiec.com", password: "password", admin: true)
            expect(user.errors[:name]).to include("is too short (minimum is 3 characters)") 
 	end 
-	it "is invalid with a name bigger than 50"
+	it "is invalid with a name bigger than 50"  do 
+		long_name = "A" * 41
+		 user = User.create(name: long_name, email: "alex3@alexswiec.com", password: "password", admin: true)
+           expect(user.errors[:name]).to include("is too long (maximum is 40 characters)") 
+	end 
 	it "is invalid without an email"
 	it "is invalid with an email longer than 105 characters"
 	it "is invalid without a valid email"

37463a5241f027d00983598c70388db2b82f6a1a
swiecone <alejandro.swiec@gmail.com>
2016-07-12 01:01:13 +0200

    changed all certificate spec to use factories

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -1,6 +1,6 @@
 class Certificate < ActiveRecord::Base
 	belongs_to :mooc
-	validates :name, presence: true, length: { minimum: 3, maximum: 25 }
+	validates :name, presence: true, length: { minimum: 3, maximum: 50 }
 	validates :url, presence: true
 	validates :licenceid, presence: true 
 end
--- spec/models/certificate_spec.rb
+++ spec/models/certificate_spec.rb
@@ -8,37 +8,32 @@ describe Certificate do
 	it "is invalid without name" do 
 		certificate = build(:certificate, name: nil)
 		certificate.valid?
-		debugger
 		expect(certificate.errors[:name]).to include("can't be blank", "is too short (minimum is 3 characters)")
 	end 
 
 	it "is invalid with name less than 3" do 
-		certificate = Certificate.new(name: "pm", 
-						url: "http://www.certificate.com",
-						licenceid: "421332423424")
-		expect(certificate).not_to be_valid
+		certificate = build(:certificate, name: "AB")
+		certificate.valid?
+		expect(certificate.errors[:name]).to include("is too short (minimum is 3 characters)")
 	end 
 
-	it "is invalid with name more than 25" do 
-		name = "A" * 26
-		certificate = Certificate.new(name: name, 
-						url: "http://www.certificate.com",
-						licenceid: "421332423424")
-		expect(certificate).not_to be_valid
+	it "is invalid with name more than 50" do 
+		name = "A" * 51
+		certificate = build(:certificate, name: name)
+		certificate.valid?
+		expect(certificate.errors[:name]).to include("is too long (maximum is 50 characters)")
 	end 
 
 	it "is invalid without url" do 
-		certificate = Certificate.new(name: "name", 
-				url: nil,	
-				licenceid: "421332423424")
-		expect(certificate).not_to be_valid
+		certificate = build(:certificate, url: nil)
+		certificate.valid?
+		expect(certificate.errors[:url]).to include("can't be blank")
 	end 
 
 	it "is invalid without licenceid" do 
-		certificate = Certificate.new(name: "pmp certificate", 
-						url: "http://www.certificate.com",
-						licenceid: nil)
-		expect(certificate).not_to be_valid
+		certificate = build(:certificate, licenceid: nil)
+		certificate.valid?
+		expect(certificate.errors[:licenceid]).to include("can't be blank")
 	end 
 
 

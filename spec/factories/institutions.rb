be8ff7cd098f016e9bad05d9d6edeb9dabc726b1
swiecone <alejandro.swiec@gmail.com>
2016-07-12 10:26:28 +0200

    created Factory for institution and then updated institution spec so we use factories with this

--- spec/factories/institutions.rb
+++ spec/factories/institutions.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do 
 	factory :institution do 
-		
+		name "San Jose University"
+		url "http://www.sanjose.com"
+		thumburl "san-jose-thumb.jpg"
 	end 
 end
--- spec/models/institution_spec.rb
+++ spec/models/institution_spec.rb
@@ -8,60 +8,50 @@ require 'rails_helper'
 
 describe Institution do 
 	it "is valid with name, url and thumbnail" do 
-		institution = Institution.new(name: "Harvard", 
-						url: "http://www.harvard.com",
-						thumburl: "http://www.thanks.com")
-		expect(institution).to be_valid
+		expect(build(:institution)).to be_valid
 	end 
 
 	it "is invalid without name" do 
-		institution = Institution.new(name: nil, 
-						url: "http://www.harvard.com",
-						thumburl: "http://www.thanks.com")
-		expect(institution).not_to be_valid
+		institution = build(:institution, name: nil)
+		institution.valid?	
+		expect(institution.errors[:name]).to include("can't be blank", "is too short (minimum is 3 characters)")
 	end 
 
 	it "is invalid with name less than 3" do 
-		institution = Institution.new(name: "HA", 
-						url: "http://www.harvard.com",
-						thumburl: "http://www.thanks.com")
-		expect(institution).not_to be_valid
+		institution = build(:institution, name: "AB")
+		institution.valid?	
+		expect(institution.errors[:name]).to include("is too short (minimum is 3 characters)")
 	end 
 
 	it "is invalid with name more than 50" do 
 		long_name = "A" * 51
-		institution = Institution.new(name: long_name, 
-						url: "http://www.harvard.com",
-						thumburl: "http://www.thanks.com")
-		expect(institution).not_to be_valid
+		institution = build(:institution, name: long_name)
+		institution.valid?	
+		expect(institution.errors[:name]).to include("is too long (maximum is 50 characters)")
 	end 
 
 	it "is invalid without url" do 
-		institution = Institution.new(name: "Harvard", 
-						url: nil,
-						thumburl: "http://www.thanks.com")
-		expect(institution).not_to be_valid
+		institution = build(:institution, url: nil)
+		institution.valid?	
+		expect(institution.errors[:url]).to include("can't be blank", "is too short (minimum is 11 characters)")
 	end 
 
 	it "is invalid with url less than 11" do 
-		institution = Institution.new(name: "Harvard", 
-						url: "http://www",
-						thumburl: "http://www.thanks.com")
-		expect(institution).not_to be_valid
+		institution = build(:institution, url: "url")
+		institution.valid?	
+		expect(institution.errors[:url]).to include("is too short (minimum is 11 characters)")
 	end 
 
 	it "is invalid without thumburl" do 
-		institution = Institution.new(name: "Harvard", 
-						url: "http://www.harvard.com",
-						thumburl: nil)
-		expect(institution).not_to be_valid
+		institution = build(:institution, thumburl: nil)
+		institution.valid?	
+		expect(institution.errors[:thumburl]).to include("can't be blank", "is too short (minimum is 11 characters)")
 	end 
 
-	it "is invalid with thumburl less than 11" do 
-		institution = Institution.new(name: "Harvard", 
-						url: "http://www.harvard.com",
-						thumburl: "http://www")
-		expect(institution).not_to be_valid
+	it "is invalid with url less than 11" do 
+		institution = build(:institution, thumburl: "thumburl")
+		institution.valid?	
+		expect(institution.errors[:thumburl]).to include("is too short (minimum is 11 characters)")
 	end 
 
 

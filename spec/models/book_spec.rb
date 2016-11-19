6a182f23b0cab8db6e597174d7070f0019323c8e
swiecone <alejandro.swiec@gmail.com>
2016-11-19 15:24:49 +0100

    removed restrictions from model for book book.rb that did not make sense, and updated book_spec.rb so that tests passed again

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -35,37 +35,20 @@ describe Book do
 	it "is not valid without title" do 
 			book = build(:book, title: nil)
 			book.valid?
-			expect(book.errors[:title]).to include("can't be blank", "is too short (minimum is 5 characters)")
+			expect(book.errors[:title]).to include("can't be blank")
 	end 
 
 	it "is not valid without shortsummary" do 
 			book = build(:book, shortsummary: nil)
 			book.valid?
-			expect(book.errors[:shortsummary]).to include("can't be blank", "is too short (minimum is 30 characters)")
+			expect(book.errors[:shortsummary]).to include("can't be blank")
 	end 
 
-	it "has a shortsummary of less than 30 characters" do 
-			book = build(:book, shortsummary: "ABC")
-			book.valid?
-			expect(book.errors[:shortsummary]).to include("is too short (minimum is 30 characters)")
-	end 
 
-	it "has a shortsummary of more than 400 characters" do 
-			@shortsummary = "A" * 401
-			book = build(:book, shortsummary: @shortsummary)
-			book.valid?
-			expect(book.errors[:shortsummary]).to include("is too long (maximum is 400 characters)")
-	end 
 
 	it "is not valid without thumburl" do 
 			book = build(:book, thumburl: nil)
 			book.valid?
-			expect(book.errors[:thumburl]).to include("can't be blank", "is too short (minimum is 11 characters)")
-	end 
-
-	it "is not valid with thumburl less thank 10 characters" do 
-			book = build(:book, thumburl: "http://www")
-			book.valid?
-			expect(book.errors[:thumburl]).to include("is too short (minimum is 11 characters)")
+			expect(book.errors[:thumburl]).to include("can't be blank")
 	end 
 end

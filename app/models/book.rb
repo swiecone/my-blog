fc606c149c9c5e5bfdb419fd7936751e72e12cda
swiecone <alejandro.swiec@gmail.com>
2016-11-19 22:46:40 +0100

    changed model book so there are less restrictions

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,8 @@
 class Book < ActiveRecord::Base
 	has_many :categories
 	validates :isbn, presence: true, length: { minimum: 10, maximum: 13 }, uniqueness: true
-	validates :title, presence: true 
+	validates :title, presence: true 	
 	validates :shortsummary, presence: true
 	validates :status, presence: true
-	validates :thumburl, presence: true 
+	validates :thumburl, presence: true ,  length: { minimum: 11 }
 end

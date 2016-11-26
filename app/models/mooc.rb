bdf6d4ab7571dc98bd30c08871ece64f2a62673b
swiecone <alejandro.swiec@gmail.com>
2016-11-26 22:28:00 +0100

    removed bothering restrictions for model that are only used by me and make my life miserable. So better remove and update test scripts

--- app/models/mooc.rb
+++ app/models/mooc.rb
@@ -3,11 +3,11 @@ class Mooc < ActiveRecord::Base
    has_one :certificate
    belongs_to :platform
    belongs_to :institution
-   validates :title, presence: true ,  length: { minimum: 5, maximum: 100 }
-   validates :shortsummary, presence: true ,  length: { minimum: 30, maximum: 400 }
+   validates :title, presence: true 
+   validates :shortsummary, presence: true 
    validates :platform_id, presence: true 
    validates :url, presence: true, length: { minimum: 11 }
    validates :institution_id, presence: true  
-   validates :thumburl, presence: true, length: { minimum: 11 }
+   validates :thumburl, presence: true
 
 end
--- spec/models/mooc_spec.rb
+++ spec/models/mooc_spec.rb
@@ -8,41 +8,10 @@ describe Mooc do
   it "is invalid without a title" do 
       mooc = build(:mooc, title: nil)
       mooc.valid?
-      expect(mooc.errors[:title]).to include("can't be blank", "is too short (minimum is 5 characters)") 
+      expect(mooc.errors[:title]).to include("can't be blank") 
    end 
 
-  it "is invalid with a title shorter than 5" do 
-     mooc = build(:mooc, title: "ABC")
-     mooc.valid?
-    expect(mooc.errors[:title]).to include("is too short (minimum is 5 characters)") 
-  end 
-
-  it "is invalid with a title longer than 100" do 
-      title = "A" * 101
-      mooc = build(:mooc, title: title)
-      mooc.valid?
-      expect(mooc.errors[:title]).to include("is too long (maximum is 100 characters)") 
-    end 
-
-  it "is invalid without a short summary" do 
-      mooc = build(:mooc, shortsummary: nil)
-      mooc.valid?
-      expect(mooc.errors[:shortsummary]).to include("can't be blank", "is too short (minimum is 30 characters)") 
-   end 
-
-  it "is invalid with a short summary less than 30" do 
-      mooc = build(:mooc, shortsummary: "This summary is invalid!")
-      mooc.valid?
-      expect(mooc.errors[:shortsummary]).to include("is too short (minimum is 30 characters)") 
-   end 
-
-  it "is invalid with a short summary more than 400" do 
-    long_summary = "A" * 401
-      mooc = build(:mooc, shortsummary: long_summary)
-      mooc.valid?
-      expect(mooc.errors[:shortsummary]).to include("is too long (maximum is 400 characters)") 
-   end 
-
+ 
   it "is invalid without a url" do 
       mooc = build(:mooc, url: nil)
       mooc.valid?
@@ -60,13 +29,7 @@ describe Mooc do
   it "is invalid without a thumburl" do 
       mooc = build(:mooc, thumburl: nil)
       mooc.valid?
-      expect(mooc.errors[:thumburl]).to include( "can't be blank" , "is too short (minimum is 11 characters)") 
-  end 
-
-  it "is invalid with a thumburl shorter than 11" do 
-      mooc = build(:mooc, thumburl: "url")
-      mooc.valid?
-      expect(mooc.errors[:thumburl]).to include( "is too short (minimum is 11 characters)") 
+      expect(mooc.errors[:thumburl]).to include( "can't be blank") 
   end 
 
   it "is invalid without a platform_id" do 
